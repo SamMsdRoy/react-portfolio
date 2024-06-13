02182976f1e7196eb97c6b6d94d84fc7dce0feac
SamMsdRoy <samvicky650@gmail.com>
2024-06-13 22:28:39 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
+import React,{ useState } from 'react'
 import './App.css'
 import {Navigationbar} from "./Components/Navbar"
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-    <Navigationbar/>
+ oops something went wrong
     </>
   )
 }
